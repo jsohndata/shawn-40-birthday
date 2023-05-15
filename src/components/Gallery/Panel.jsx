9c98251cd90fc8b49ba0400605a4696f08cbc233
jSohnData <jihos@me.com>
2023-05-15 17:10:09 -0400

    UPDATES youtube modal

--- src/components/Gallery/Panel.jsx
+++ src/components/Gallery/Panel.jsx
@@ -3,18 +3,31 @@ import { Col,Image, Modal } from "react-bootstrap";
 
 export default function Panel ( {data: {id, cover, large}} ) {
 
-  const [show, setShow] = useState(false);
+  const[showModal, setShowModal] = useState(false);
 
-  const toggleShow = () => setShow(!show);
+  const toggleShowModal = () => setShowModal(!showModal);
 
   return (
     <Col xs={6} sm={4} md={3} lg={2} className="m-0 m-xl-1 p-0 column">
-      <a href={large} target="_blank"><Image 
+      <Image 
         src={`${cover}`} 
         alt={id}        
         fluid
-        className="button-effect"/></a>
-      
+        className="button-effect"
+        onClick={toggleShowModal} />
+      <Modal
+            size="lg"
+            show={showModal}
+            onHide={toggleShowModal}
+            className="backdrop-effect modal-container">
+            <iframe 
+              style={{width: '100%', height: '70vh'}}               
+              src={large} 
+              frameborder="0" 
+              allow="autoplay; encrypted-media" 
+              title="example iFrame Equalize Digital Home Page"
+              allowfullscreen></iframe>
+      </Modal>    
     </Col>
   )
 }
